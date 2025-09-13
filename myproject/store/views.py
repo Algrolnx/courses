8053b813df8318b825000cbe229613bd4fe55b43
Yehor <bizonheroes999@gmail.com>
2025-09-13 15:05:35 +0300

    before api

--- myproject/store/views.py
+++ myproject/store/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Course
 
 def index(request):
     courses = Course.objects.all()
-    return render(request, 'courses.html', {'courses': courses})
+    return render(request, 'store/courses.html', {'courses': courses})
 
 def single_course(request, course_id):
-    course = Course.objects.get(pk=course_id)
-    return render(request, 'single_course.html', {'course': course})
+    course = get_object_or_404(Course, pk=course_id)
+    return render(request, 'store/single_course.html', {'course': course})
